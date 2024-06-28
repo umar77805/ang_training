ba7f6d748b21cce49fe144ac471a0d597e3000c4
Mohammad Umar Ahmed <mohdumar@lentra.ai>
2024-06-28 12:23:22 +0530

    added component communication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { LoginComponent } from './login/login.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [LoginComponent],
+  imports: [LoginComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class App {
-  name = 'Angular';
+  isSubmitted = false;
+
+  handleSubmit = () => {
+    this.isSubmitted = true;
+  }
+
+  handleSubmitTextClick = () => {
+    this.isSubmitted = false;
+  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
-import { InputComponent } from '../input/input.component';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
+import { InputComponent } from '../input/input.component';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [InputComponent, NgFor],
+  imports: [InputComponent, NgFor, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  @Output() submitted = new EventEmitter<void>();
   formInputData = [
     {
       title: "Email",
@@ -21,6 +23,6 @@ export class LoginComponent {
     },
   ]
   onFormSubmit = () => {
-    alert('Clicked');
+    this.submitted.emit();
   };
 }
